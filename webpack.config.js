79fd1e505c60971ef510bd68952ebc532c8c6558
dadawangweb <1024564775@qq.com>
2017-11-01 17:05:23 +0800

    删除bundle.js
    reson：没有作用，打包生成的是main.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.tpl.html',       // 根据魔板自动生成index.html
+            template: './index.html',       // 根据魔板自动生成index.html
             inject: 'body',
             filename: './index.html'
         }),
